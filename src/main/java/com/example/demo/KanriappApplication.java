583c72dd108a9ae600e27c7ef831f3dabd4c9937
Mint <Mint@192.168.0.12>
2021-02-05 15:39:13 +0900

    製品の管理をするアプリケーションです。

---
+++ src/main/java/com/example/demo/KanriappApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanriappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanriappApplication.class, args);
+	}
+
+}
